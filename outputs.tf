cd0d5d844ad583f5789f850c888e7930e3dfeeba
Anand_d <anandrl120@gmail.com>
2026-07-14 12:24:36 +0530

    Fix formatting of output values in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -84,7 +84,7 @@ output "bastion_sg_id" {
 
 output "alb_sg_id" {
   description = "ID of the ALB security group"
-  value       = aws_security_group.alb_sg.id
+  value      = aws_security_group.alb_sg.id
 }
 
 output "app_sg_id" {
